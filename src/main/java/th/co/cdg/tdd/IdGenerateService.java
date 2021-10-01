f415b5af90e85d19cd34bf6f25202fb74abcf015
tetaearnit <arnit.musikamas@gmail.com>
2021-10-01 08:59:41 +0700

    broken test

---
+++ src/main/java/th/co/cdg/tdd/IdGenerateService.java
@@ -0,0 +1,14 @@
+package th.co.cdg.tdd;
+
+import java.util.Random;
+
+public class IdGenerateService {
+
+    public String getData() {
+        // Dependency
+        Random random = new Random();
+        int id = random.nextInt(10);
+        // Result
+        return "CODE" + id;
+    }
+}
---
+++ src/test/java/th/co/cdg/tdd/IdGenerateServiceTest.java
@@ -0,0 +1,15 @@
+package th.co.cdg.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IdGenerateServiceTest {
+
+    @Test
+    public void getData() {
+        IdGenerateService service = new IdGenerateService();
+        String result = service.getData();
+        assertEquals("CODE6", result);
+    }
+}
